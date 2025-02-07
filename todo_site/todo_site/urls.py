28f5fbe368d71572fa5c3d084bc2774e0a7aca55
Alexey Lvov <alexlvov@yandex.ru>
2025-02-07 12:54:35 +0300

    add urls in urls.py

--- todo_site/todo_site/urls.py
+++ todo_site/todo_site/urls.py
@@ -18,5 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('', views.index, name="todo"),
+    path('del/<str:item_id>', views.remove, name="del"),
     path('admin/', admin.site.urls),
 ]
